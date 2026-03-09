754308133979d79d728f839119b8a7cffce23fff
Фетюков Никита <nikfet24@gmail.com>
2026-03-10 00:48:03 +0300

    chore: fix route

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/reading" element={<FullText />} />
